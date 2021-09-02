97a0eb6d7b7a24986aae99d476b1835cefbb9806
mboudet <mateo.boudet@gmail.com>
2021-09-02 17:36:07 +0200

    Tmp

---
+++ braskolib/datafile.py
@@ -0,0 +1,222 @@
+from pathlib import Path
+import os
+import json
+from urllib.parse import quote
+
+import pandas
+import numpy as np
+
+
+class Datafile():
+
+    def __init__(self, pattern, integration_file, conversion_data, search_folder, temp_folder, askomics_client, gopublish_data={}, data_files={}):
+        self.pattern = pattern
+        self.search_folder = search_folder
+        self.data_files = data_files
+        self.files = {}
+        self.conversion_data = conversion_data
+        self.askomics_client = askomics_client
+        self.gopublish_data = gopublish_data
+        self.files_to_integrate = []
+        self.temp_folder = temp_folder
+
+        if any([file.get("gopublish", False) for file in self.data_files]) and not gopublish_data:
+            raise Exception("Requested file publication but gopublish_data is not set")
+
+        if "replace_name" in conversion_data and not len(conversion_data['replace_name']) == 2:
+            raise Exception("replace_name must contain two values: the current string, and the string to replace")
+        # Conversion data: Dict with keys:
+        # sheet: 0 (optional, default to 0)
+        # add_columns: {"values": ["XXX"], "from_path": ["XXX"]}
+        # drop_columns: [{column: "XXX", "before": "xxx", "after":xxx}]
+        # new_uri: 'base_id_uri'
+        # temp_base_name: "xxx"
+
+        with open(integration_file) as datafile:
+            self.integration_template = json.load(datafile)
+            self.entity = self.integration_template["headers"][0]
+
+        self.files = {}
+        self.files_to_integrate = []
+        self.latest_entity = False
+
+    def get_files(self):
+        file_dict = {}
+        filtered_file_dict = {}
+        for path in Path(self.search_folder).rglob(self.pattern):
+            head, tail = os.path.split(path)
+            if "Forms" in head:
+                continue
+            if head not in file_dict:
+                file_dict[head] = []
+            file_dict[head].append(tail)
+        for path, names in file_dict.items():
+            latest_file = max([os.path.join(path, name) for name in names], key=os.path.getctime)
+            head, tail = os.path.split(latest_file)
+            filtered_file_dict[head] = {"file": tail, "time": os.path.getctime(latest_file)}
+
+        self.files = filtered_file_dict
+
+    def cleanup_askomics(self):
+        datasets = self.askomics_client.dataset.list()
+        files = self.askomics_client.file.list()
+
+        names_to_delete = []
+        files_to_delete = []
+        keys_to_delete = []
+
+        for key, value in self.files.items():
+            file_name = value["file"].replace(".ods", ".csv")
+            for file in files:
+                if file["name"] == file_name:
+                    if file["date"] < value["time"]:
+                        files_to_delete.append(file["id"])
+                        names_to_delete.append(file_name)
+                        print("Deleting remote obsolete file " + file_name)
+                    else:
+                        print("Skipping older local file " + file_name)
+                        keys_to_delete.append(key)
+
+        datasets_to_delete = [dataset["id"] for dataset in datasets if dataset["name"] in names_to_delete]
+
+        if datasets_to_delete:
+            self.askomics_client.dataset.delete(datasets_to_delete)
+        if files_to_delete:
+            self.askomics_client.file.delete(files_to_delete)
+
+        if keys_to_delete:
+            for key in keys_to_delete:
+                self.files.pop(key, None)
+
+    def convert_files(self):
+        for path, file in self.files.keys():
+            self._convert_file(path, file)
+
+    def upload_asko(self):
+        for file_path in self.files_to_integrate:
+            self.askomics_client.file.upload(file_path=file_path)
+
+    def integrate_asko(self):
+        uploaded_files = set([os.path.basename(file) for file in self.files_to_integrate])
+        files = self.askomics_client.file.list()
+        file_ids = set()
+
+        for file in files:
+            if file["name"] in uploaded_files:
+                file_ids.add(file["id"])
+        for id in file_ids:
+            self.askomics_client.file.integrate_csv(id, columns=self.integration_template["columns"], headers=self.integration_template["headers"])
+
+    def _convert_file(self, file_path, file_name):
+        # add_id must be a dict, containing a column name with key "column", and optional list of values added (key "values")
+        full_path = os.path.join(file_path, file_name)
+        convert_data = self.conversion_data
+        df = pandas.read_excel(full_path, sheet_name=convert_data.get("sheet", 0))
+        col_to_del = set()
+        if len(df) == 0:
+            return
+
+        if self.subset:
+            index = df.columns.get_loc(self.subset["column"])
+            subdf = df.iloc[:, 0:index+1].copy()
+            for col in self.subset.get("add_columns", []):
+                subdf[col] = col
+            subdf.to_csv(self.subset["temp_path"], index=False, sep="\t")
+            self.files_to_integrate.append(self.subset["temp_path"])
+            df.drop(df.iloc[:, 1:index+1], inplace=True, axis=1)
+
+        if convert_data.get("new_uri"):
+            if not self.latest_entity:
+                self._set_last_entity()
+            new_col_id = np.arange(self.latest_entity, len(df) + self.latest_entity)
+            df.insert(loc=0, column="tmp_col_id", value=new_col_id)
+            df.insert(loc=0, column="tmp_col_uri", value=convert_data.get("new_uri") + "_")
+
+            df["tmp_col_uri"] = df["tmp_col_uri"] + np.arange(self.latest_entity, len(df) + self.latest_entity).astype(str)
+            self.latest_entity += len(df)
+
+        for path in self.data_files:
+            if path.get("col_base_path"):
+                df[path["col"]] = df[[path["col_base_path"], path["col"]]].agg('/'.join, axis=1)
+                if path.get("delete_base"):
+                    col_to_del.add(path["col_base_path"])
+            if path.get("gopublish"):
+                base_path = path.get("base_path")
+                df[path["col"]] = df[path["col"]].apply(lambda x: self._get_file_uri(self.gopublish_data, base_path, x))
+
+        # drop_columns: [{column: "XXX", "before": "xxx", "after":xxx}]
+        for column in convert_data.get("drop_columns", []):
+            if column.get("column"):
+                df = df.drop(column.get("column"), 1)
+
+            if column.get("before") and column.get("after"):
+                before_loc = df.columns.get_loc(column.get("before"))
+                after_loc = df.columns.get_loc(column.get("after"))
+                # Check for +1 needed
+                if before_loc > after_loc:
+                    df = df.drop(df.iloc[:, after_loc + 1:before_loc - 1], 1)
+                else:
+                    df = df.loc[:, before_loc:after_loc]
+            else:
+                if column.get("before"):
+                    loc = df.columns.get_loc(column.get("before"))
+                    df = df.loc[:, loc:]
+                if column.get("after"):
+                    loc = df.columns.get_loc(column.get("after"))
+                    df = df.loc[:, :loc]
+
+        for col in col_to_del:
+            df = df.drop(col, 1)
+
+        if convert_data['replace_name']:
+            file_name = file_name.replace(convert_data['replace_name'][0], convert_data['replace_name'][1])
+        else:
+            file_name = file_name.replace(".ods", ".csv")
+
+        new_file_path = os.path.join(self.temp_folder, file_name)
+
+        df.to_csv(new_file_path, index=False, sep="\t")
+        self.files_to_integrate.append(new_file_path)
+
+    def _set_last_entity(self):
+
+        config = self.askomics_client.sparql.info()
+        entity_name = quote(self.entity)
+
+        graphs = [key for key in config["graphs"]]
+        endpoints = [key for key in config["endpoints"]]
+
+        if not (config["graphs"] and config["endpoints"]):
+            self.latest_entity = 0
+            return
+
+        query = "SELECT DISTINCT MAX(?temp_var_id) as ?output_var\nWHERE {\n"
+        query += "?temp_var_uri rdf:type <{}{}> .\n".format(config["namespace_data"], entity_name)
+        query += "?temp_var_uri <{}id> ?temp_var_id .\n".format(config["namespace_data"])
+        query += "}"
+
+        result = self.askomics_client.sparql.query(query, graphs, endpoints)
+
+        if not result.get("data") or not result["data"][0].get("output_var"):
+            self.latest_entity = 0
+        else:
+            self.latest_entity = int(result["data"][0]["output_var"]) + 1
+
+    def _get_file_uri(self, base_path, file_path):
+        gopublic_client = self.gopublish_data['client']
+        token = self.gopublish_data['token']
+        base_url = self.gopublish_data['base_url']
+        file_name = os.path.basename(file_path)
+        data = gopublic_client.file.search(file_name)
+        if data["files"]:
+            return base_url + data["files"][0]["uri"]
+
+        if base_path:
+            file_path = os.path.join(base_path, file_path)
+
+        if not os.path.exists(file_path):
+            print("Missing file {}".format(file_path))
+            return file_path
+
+        data = gopublic_client.file.publish(file_path, token=token)
+        return base_url + data["file_id"]
--- load.py
+++ load.py
@@ -1,156 +1,85 @@
-from pathlib import Path
 import os
 import json
 import glob
-from urllib.parse import quote
 
-import pandas
-import numpy as np
 import askoclics
 import gopublic
 
-# Get list of files matching pattern, keeping the latest one when there are two in the same folder
-def get_files(pattern, root_path):
-    file_dict = {}
-    filtered_file_dict = {}
-    for path in Path(root_path).rglob(pattern):
-        head, tail = os.path.split(path)
-        if "Forms" in head:
-            continue
-        if not head in file_dict:
-            file_dict[head] = []
-        file_dict[head].append(tail)
-    for path, names in file_dict.items():
-        latest_file = max([os.path.join(path, name) for name in names], key=os.path.getctime)
-        head, tail = os.path.split(latest_file)
-        filtered_file_dict[head] = {"file": tail, "time": os.path.getctime(latest_file)}
-    return filtered_file_dict
+from braskoload.braskolib.datafile import Datafile
 
-def convert_file(file_path, temp_path, entity_dict, subset={}, add_id="", entity_name="", asko_client=None, gopublic_data=None, paths=[], sheet=0):
-    # add_id must be a dict, containing a column name with key "column", and optional list of values added (key "values")
-    df = pandas.read_excel(file_path, sheet_name=sheet)
-    col_to_del = set()
-    if len(df) == 0:
-        return False, entity_dict
-
-    if subset:
-        index = df.columns.get_loc(subset["column"])
-        subdf = df.iloc[:, 0:index+1].copy()
-        for col in subset.get("add_columns", []):
-            subdf[col] = col
-        subdf.to_csv(subset["temp_path"], index=False, sep="\t")
-        df.drop(df.iloc[:, 1:index+1], inplace=True, axis=1)
-
-    if add_id:
-        if not add_id in entity_dict:
-            entity_dict[add_id] = get_current_uri(asko_client, entity_name)
-
-        new_col_id = np.arange(entity_dict[add_id], len(df) + entity_dict[add_id])
-        df.insert(loc=0, column="tmp_col_id", value=new_col_id)
-        df.insert(loc=0, column="tmp_col_uri", value=add_id + "_")
-
-        df["tmp_col_uri"] = df["tmp_col_uri"] + np.arange(entity_dict[add_id], len(df) + entity_dict[add_id]).astype(str)
-        entity_dict[add_id] += len(df)
-
-    for path in paths:
-        if path.get("base"):
-            df[path["col"]] = df[[path["base"], path["col"]]].agg('/'.join, axis=1)
-            if path.get("delete_base"):
-                col_to_del.add(path["base"])
-
-
-    for col in col_to_del:
-        df = df.drop(col, 1)
-
-    df.to_csv(temp_path, index=False, sep="\t")
-    return True, entity_dict
-
-def get_current_uri(asko_client, entity_name):
-
-    config = asko_client.sparql.info()
-    entity_name = quote(entity_name)
-
-    graphs = [key for key in config["graphs"]]
-    endpoints = [key for key in config["endpoints"]] 
-
-    if not (config["graphs"] and config["endpoints"]):
-        return 0
-
-    query = "SELECT DISTINCT MAX(?temp_var_id) as ?output_var\nWHERE {\n"
-    query += "?temp_var_uri rdf:type <{}{}> .\n".format(config["namespace_data"], entity_name)
-    query += "?temp_var_uri <{}id> ?temp_var_id .\n".format(config["namespace_data"])
-    query += "}"
-
-    result = asko_client.sparql.query(query, graphs, endpoints)
-
-    if not result.get("data") or not result["data"][0].get("output_var"):
-        return 0
-    else:
-        return int(result["data"][0]["output_var"]) + 1
-
-def get_file_uri(gopublic_data, file_path):
-    gopublic_client = gopublic_data['client']
-    token = gopublic_client['token']
-    base_url = gopublic_client['base_url']
-    file_name = os.path.basename(file_path)
-    data = gopublic_client.file.search(file_name)
-    if data:
-        return base_url + data[0]["uri"]
-
-    data = gopublic_client.file.publish(file_path, token=token)
-    return return base_url + data["file_id"]
-
-
-def upload_asko(asko_client, file_path):
-    asko_client.file.upload(file_path=file_path)
-
-def integrate_asko(asko_client, file_name, json_data):
-    files = asko_client.file.list()
-    file_id = None
-
-    for file in files:
-        if file["name"] == file_name:
-            if not file_id:
-                file_id = file["id"]
-            if file_id and file_id < file["id"]:
-                file_id = file["id"]
-    if file_id:
-        asko_client.file.integrate_csv(file_id, columns=json_data["columns"], headers=json_data["headers"])
-
-def asko_cleanup(asko_client, data):
-
-    datasets = asko_client.dataset.list()
-    files = asko_client.file.list()
-
-    names_to_delete = []
-    files_to_delete = []
-    keys_to_delete = []
-
-    for key, value in data.items():
-        file_name = value["file"].replace(".ods", ".csv")
-        for file in files:
-            if file["name"] == file_name:
-                if file["date"] < value["time"]:
-                    files_to_delete.append(file["id"])
-                    names_to_delete.append(file_name)
-                    print("Deleting remote obsolete file " + file_name)
-                else:
-                    print("Skipping older local file " + file_name)
-                    keys_to_delete.append(key)
-
-    datasets_to_delete = [dataset["id"] for dataset in datasets if dataset["name"] in names_to_delete]
-
-    if datasets_to_delete:
-        asko_client.dataset.delete(datasets_to_delete)
-    if files_to_delete:
-        asko_client.file.delete(files_to_delete)
-
-    if keys_to_delete:
-        for key in keys_to_delete:
-            data.pop(key, None)
-    return data
 
 def main():
+
+    gopublish_data = {
+        "client": gopublic.GopublishInstance(url="", username="", password=""),
+        "token": "",
+        "base_url": ""
+    }
+
+    asko_client = askoclics.AskomicsInstance(url="http://192.168.100.87", api_key="")
+
+    datasets = [
+        Datafile(
+          pattern="Population_description*W.ods",
+          integration_file="templates/population_wild_asko.json",
+          conversion_data={"sheet": 2, "new_uri": "wild_population", "drop_columns": {"after": "Population name", "before": "Area (1)"}},
+          search_folder="/groups/brassica/db/projects/BrasExplor",
+          temp_folder="tmp/",
+          askomics_client=asko_client,
+          subset={
+            "integration_file": "templates/population_base_asko.json",
+            "conversion_data": {
+                "replace_name": [".csv", "_base.csv"],
+                "add_columns": [{"from_path": -2, "convert": ["_", " "]}, {"from_path": -3}],
+                "drop_columns": {"after": "Altitude"}
+            }
+          }
+        ),
+        Datafile(
+          pattern="Population_description*L.ods",
+          integration_file="templates/population_lr_asko.json",
+          conversion_data={"sheet": 2, "new_uri": "landrace_population"},
+          search_folder="/groups/brassica/db/projects/BrasExplor",
+          temp_folder="tmp/",
+          askomics_client=asko_client
+        ),
+        Datafile(
+          pattern="Botanical_species*.ods",
+          integration_file="templates/botanical_asko.json",
+          conversion_data={"sheet": 2, "new_uri": "botanical_density"},
+          search_folder="/groups/brassica/db/projects/BrasExplor",
+          temp_folder="tmp/",
+          askomics_client=asko_client
+        ),
+        Datafile(
+          pattern="Pictures*.ods",
+          integration_file="templates/picture_asko.json",
+          conversion_data={"sheet": 2, "new_uri": "picture"},
+          search_folder="/groups/brassica/db/projects/BrasExplor",
+          temp_folder="tmp/",
+          askomics_client=asko_client
+        ),
+        Datafile(
+          pattern="test_asko.ods",
+          integration_file="/home/genouest/genouest/mboudet/test_asko",
+          conversion_data={"new_uri": "sequence"},
+          search_folder="/groups/brassica/db/projects/BrasExplor",
+          temp_folder="tmp/",
+          askomics_client=asko_client,
+          gopublish_data=gopublish_data,
+          data_files=[{
+            "col": "Nom fichier read R1",
+            "col_base_path": "Chemin stockage séquences bulks sur Genouest",
+            "delete_base": True,
+            "gopublish": True
+            }, {
+            "col": "Nom fichier read R2",
+            "col_base_path": "Chemin stockage séquences bulks sur Genouest",
+            "delete_base": True,
+            "gopublish": True
+          }]
+        )
+    ]
 
     patterns = {
         "Population_description*W.ods": {
@@ -179,17 +108,10 @@ def main():
             "sheet": 0,
             "integration": "templates/sequence_asko.json",
             "new_id": "sequence",
-            "paths": [{"col": "Nom fichier read R1", "base": "Chemin stockage séquences bulks sur Genouest", "delete_base": True}, {"col":"Nom fichier read R2", "base": "Chemin stockage séquences bulks sur Genouest" , "delete_base": True}]
+            "paths": [{"col": "Nom fichier read R1", "base": "Chemin stockage séquences bulks sur Genouest", "delete_base": True, "generate_uri": True}, {"col": "Nom fichier read R2", "base": "Chemin stockage séquences bulks sur Genouest", "delete_base": True}]
         }
     }
 
-    gopublic_data = {
-        "client": gopublic.GopublishInstance(url="", username="", password=""),
-        "token": "",
-        "base_url": ""
-    }
-
-    asko_client = askoclics.AskomicsInstance(url="http://192.168.100.87", api_key="")
     # Cleanup
     files = glob.glob('tmp/*')
     for f in files:
@@ -206,7 +128,6 @@ def main():
 
         for path, file in data.items():
             filename = file["file"]
-            base_name = filename.replace(".ods", "")
             new_file_name = filename.replace(".ods", ".csv")
             with open(validation_files["integration"]) as datafile:
                 asko_data = json.load(datafile)
@@ -241,6 +162,6 @@ def main():
                 upload_asko(asko_client, os.path.join("tmp/", new_file_name))
                 integrate_asko(asko_client, new_file_name, asko_data)
 
+
 if __name__ == "__main__":
     main()
-
